1a47516ff29dbf6d74832971f6370899e580ab90
HUANGV1 <viho2007@gmail.com>
2025-07-09 11:27:46 -0400

    animated and enlarged project cards on click

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const openProjectCard = document.querySelector('.project-card');
+const cards = document.querySelectorAll('.project-card');
+const overlay = document.getElementById('overlay');
 
 
-openProjectCard.addEventListener('click', ()=>{
-    
+cards.forEach(card => {
+    card.addEventListener('click', ()=>{
+        card.classList.add('active');
+        overlay.classList.add('show');
+        
+    });
+});
+
+overlay.addEventListener('click', ()=>{
+    const openCard=document.querySelector('.project-card.active');
+    if (openCard) {
+        openCard.classList.remove('active');
+        overlay.classList.remove('show');
+    }
+});
+
+document.addEventListener('keydown', e=>{
+    if (e.key === 'Escape') {
+        const openCard=document.querySelector('.project-card.active');
+        if (openCard) {
+            openCard.classList.remove('active');
+            overlay.classList.remove('show');
+        }
+    }
 });
